1c331716fd8a99fcbdc79f5a37f800ae210210dc
amrit anand <amrit.anand@e2enetworks.com>
2025-08-24 19:19:04 +0530

    added experience and contact section

---
+++ src/components/constants.js
@@ -0,0 +1,9 @@
+  const EMAILJS_SERVICE_ID = "service_wa7dz5c"; // You need to replace this
+  const EMAILJS_TEMPLATE_ID = "template_3x4vuzu"; // You need to replace this
+  const EMAILJS_PUBLIC_KEY = "TWPCVB7aS6ejx1nIs"; // You need to replace this
+
+export {
+  EMAILJS_SERVICE_ID,
+  EMAILJS_TEMPLATE_ID,
+  EMAILJS_PUBLIC_KEY
+};
